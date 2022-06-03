e3608c4dd5507ae743ea33ff6fc5cafac5ae04fa
Bryan-JR <95584508+Bryan-JR@users.noreply.github.com>
2022-06-03 14:59:09 -0500

    Add files via upload

--- ListaProductos/src/Lista.java
+++ ListaProductos/src/Lista.java
@@ -1,12 +1,14 @@
-
 import javax.swing.*;
 
 public class Lista {
     private Nodo inicio;
+    private Nodo centro;
     private Nodo fin;
     
     public Lista(){
         inicio = null;
+        centro = null;
+        fin = null;
     }
     
     public Nodo getInicio(){
@@ -35,8 +37,6 @@ public class Lista {
         return cont;
     }
     
-    
-    
     public void AgregarProducto(Nodo producto){
         if(inicio==null){//Si el inicio es vacio, se crea la lista
             inicio = producto;
@@ -46,6 +46,71 @@ public class Lista {
             fin.setEnlace(producto);
             fin = producto;
             JOptionPane.showMessageDialog(null, "Producto guardado!");
+        }
+    }
+    
+    
+    public Nodo getCentro(int medio, Nodo ref){
+        boolean encontrado = false;
+        int cont = 0;
+        while(cont != medio && !encontrado) {    
+            cont++;
+            if(cont==medio){
+                centro = ref;
+                encontrado = true;
+            }
+            
+            ref = ref.getEnlace();
+        }
+        return centro;
+    }
+    
+    public void ordenarLista(){
+        Nodo temp, temp1, ant = null;
+        int tam = getTam();
+        for (int i = 1; i < tam; i++){
+            temp = inicio;
+            temp1 = temp.getEnlace();
+            for (int j = 1; j <= tam-i; j++) {
+                if(temp.getId()>temp1.getId()){
+                    if(temp==inicio){
+                        temp.setEnlace(temp1.getEnlace());
+                        temp1.setEnlace(inicio);
+                        inicio = temp1;
+                        ant = temp1;
+                        temp1 = temp.getEnlace();
+                        if(temp.getEnlace()==null) fin = temp;
+                    } else {
+                        temp.setEnlace(temp1.getEnlace());
+                        temp1.setEnlace(temp);
+                        ant.setEnlace(temp1);
+                        temp1 = temp.getEnlace();
+                        if(temp.getEnlace()==null) fin = temp;
+                    }
+                } else {
+                    ant = temp;
+                    temp = temp.getEnlace();
+                    temp1 = temp.getEnlace();
+                }
+            }
+        }
+    }
+    
+    public void busquedaBinarina(int id){
+        int med = Math.round(getTam()/2)+1;
+        Nodo der, izq, medio=getCentro(med, inicio);
+        if(medio.getId()==id){
+             JOptionPane.showMessageDialog(null, medio.info());
+        }else if(id<medio.getId()){
+            izq = medio;
+            med = Math.round(med/2)+1;
+            medio = getCentro(med, izq);
+            busquedaBinarina(id);
+        } else if(id>medio.getId()){
+            der = medio;
+            med = Math.round(med/2)+1;
+            medio = getCentro(med, der);
+            busquedaBinarina(id);
         }
     }
 }
--- ListaProductos/src/Ventana.java
+++ ListaProductos/src/Ventana.java
@@ -23,6 +23,7 @@ public class Ventana extends javax.swing.JFrame {
     }
     
     private void mostrarTabla(){
+        lista.ordenarLista();
         int cont = 0;
         Nodo aux = lista.getInicio();
         while(aux!=null){
@@ -79,6 +80,7 @@ public class Ventana extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -157,7 +159,7 @@ public class Ventana extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -204,24 +206,36 @@ public class Ventana extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jButton2.setText("Mostrar centro");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton2))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton2)
                 .addContainerGap())
         );
 
@@ -236,6 +250,10 @@ public class Ventana extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         AgregarProducto();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        lista.busquedaBinarina(10);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -274,6 +292,7 @@ public class Ventana extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
